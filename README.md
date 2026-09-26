7dca959980e3cbc2b92a04353cf57c518ce3bede
Namrata <namratawadekar07@gmail.com>
2026-09-26 15:57:24 +0530

    Track

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CropIQ-V2
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ce54d413b31de09a8fb085b2210320dbc7ee05bdf1accdf1695588b35eee82b
+size 11
--- esp32/secrets.h
+++ esp32/secrets.h
@@ -1,2 +1,3 @@
-#define WIFI_SSID "OPPO"
-#define WIFI_PASSWORD "nrsf3263"
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc5640be75adff422c8574290445dd25e0fa68c703f52b1def89d67b357ab1dc
+size 58
